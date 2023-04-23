c9995ac516e3ac19fcf3f7e9c04d2d6b12962722
Mateusz <mat.walczak01@gmail.com>
2023-04-23 14:25:16 +0200

    Actually start the project

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+
+const int windowHeight {800};
+const int windowWidth {800};
+
+
 
 int main()
 {
-    std::cout << "Hello world";
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML works!");
+    sf::Texture chessBoard;
+    if (!chessBoard.loadFromFile("textures/chessboard.png"))
+    {
+        return 1;
+    }
+
+    sf::Sprite board;
+    board.setTexture(chessBoard);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(board);
+        window.display();
+    }
+
     return 0;
 }
